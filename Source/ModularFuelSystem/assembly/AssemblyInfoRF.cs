15f294db5cb83eba07a6c8639bb62a29d793845e
Lisias T <me@lisias.net>
2018-10-21 08:20:19 -0200

    Project Normalizing

--- Source/ModularFuelSystem/assembly/AssemblyInfoRF.cs
+++ Source/ModularFuelSystem/assembly/AssemblyInfoRF.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("ModularFuelTanks")]
+[assembly: AssemblyTitle("RealFuels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -17,8 +17,9 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("@VERSION@")]
-[assembly: AssemblyInformationalVersionAttribute("@FULL_VERSION@")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -26,3 +27,5 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+[assembly: KSPAssembly("RealFuels", 1, 0, 0)]
+[assembly: KSPAssemblyDependency("SolverEngines", 3, 3)]
