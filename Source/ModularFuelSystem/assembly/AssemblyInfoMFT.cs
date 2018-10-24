9b60bec538fc20946246758a3e95ba288f56c300
Lisias T <me@lisias.net>
2018-10-24 07:36:40 -0200

    Versioning Normalizing for Modular Fuel Tanks

--- Source/ModularFuelSystem/assembly/AssemblyInfoMFT.cs
+++ Source/ModularFuelSystem/assembly/AssemblyInfoMFT.cs
@@ -17,8 +17,9 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0")]
+[assembly: AssemblyVersion(ModularFuelTanks.Version.Number)]
+[assembly: AssemblyInformationalVersionAttribute(ModularFuelTanks.Version.Number)]
+[assembly: AssemblyFileVersion(ModularFuelTanks.Version.Number)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -26,3 +27,4 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+[assembly: KSPAssembly("ModularFuelTanks", ModularFuelTanks.Version.major, ModularFuelTanks.Version.minor)]
--- Source/ModularFuelSystem/assembly/AssemblyInfoRF.cs
+++ Source/ModularFuelSystem/assembly/AssemblyInfoRF.cs
@@ -17,9 +17,9 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0")]
-[assembly: AssemblyFileVersion("1.0")]
+[assembly: AssemblyVersion(RealFuels.Version.Number)]
+[assembly: AssemblyInformationalVersionAttribute(RealFuels.Version.Number)]
+[assembly: AssemblyFileVersion(RealFuels.Version.Number)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -27,5 +27,5 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-[assembly: KSPAssembly("RealFuels", 1, 0, 0)]
+[assembly: KSPAssembly("RealFuels", RealFuels.Version.major, RealFuels.Version.minor)]
 [assembly: KSPAssemblyDependency("SolverEngines", 3, 3)]
